e4c288e51d6158641a16743ac0ccee4f532f2c2e
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2022-04-29 23:15:56 +0600

    Added: Shop page products

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import Categories from "../components/Categories";
+import LatestProducts from "../components/LatestProducts";
 
-const Products = () => {
+const Products = ( { latestProducts } ) => {
     return (
-        <div className={'py-20'}>
-            <h2>Products</h2>
-        </div>
+        <>
+            <LatestProducts products={latestProducts} />
+        </>
     );
 };
+
+export async function getStaticProps( context ) {
+
+    const api = new WooCommerceRestApi({
+        url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+        consumerKey: process.env.WC_CONSUMER_KEY,
+        consumerSecret: process.env.WC_CONSUMER_SECRET,
+        version: "wc/v3"
+    });
+
+    try {
+
+        //latest products
+        const latestProductsResponse = await api.get('products',{ per_page: 10 });
+        const latestProducts = latestProductsResponse.data;
+
+        return {
+            props: {
+                latestProducts: latestProducts
+            }
+        }
+    } catch (error) {
+        return {
+            props: {
+                latestProducts: []
+            }
+        }
+    }
+}
 
 export default Products;
