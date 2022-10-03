9b88bd01e15dc918ec468c191542d249ba1f75d3
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2022-10-03 23:17:16 +0600

    Category link added

--- components/Category.js
+++ components/Category.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import Image from "next/image";
+import Link from 'next/link';
 
 const Category = ({category}) => {
     return (
-        <a href={'#'} className={'group'}>
-            <div
-                className="w-full rounded-lg overflow-hidden">
-                {
-                    category.image ? (
-                        <Image
-                            className="object-center object-cover group-hover:opacity-75"
-                            src={category.image.src}
-                            alt={category.name}
-                            width={500}
-                            height={500}
-                        />
-                    )
-                    :
-                    (
-                        <Image
-                            src={`/placeholder.png`}
-                            alt={category.name}
-                            className="w-full h-full object-center object-cover group-hover:opacity-75"
-                            width={500}
-                            height={500}
-                        />
-                    )
-                }
-            </div>
-            <h3 className="mt-4 text-sm text-gray-700 text-center">{`${category.name}  (${category.count})`}</h3>
-        </a>
+        <Link href={`/categories/${category.id}`}>
+            <a className={'group'}>
+                <div
+                    className="w-full rounded-lg overflow-hidden">
+                    {
+                        category.image ? (
+                            <Image
+                                className="object-center object-cover group-hover:opacity-75"
+                                src={category.image.src}
+                                alt={category.name}
+                                width={500}
+                                height={500}
+                            />
+                        )
+                        :
+                        (
+                            <Image
+                                src={`/placeholder.png`}
+                                alt={category.name}
+                                className="w-full h-full object-center object-cover group-hover:opacity-75"
+                                width={500}
+                                height={500}
+                            />
+                        )
+                    }
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700 text-center">{`${category.name}  (${category.count})`}</h3>
+            </a>
+        </Link>
     );
 };
 
