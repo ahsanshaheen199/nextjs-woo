d4ff8a1fcba3252c1a15976eac49b149ed63e6a1
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2022-11-11 23:53:03 +0600

    Update category component

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -2,6 +2,14 @@ export type Category = {
     id: number;
     name: string;
     slug: string;
+    count: number;
+    image: CategoryImage;
+}
+
+export type CategoryImage = {
+    id: number;
+    src: string;
+    alt: string;
 }
 
 export type CategoryTree = Category & {
