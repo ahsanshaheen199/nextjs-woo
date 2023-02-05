b912ad3970bfabd8eb8c713af544a6d8c0aa2158
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2023-02-05 23:03:03 +0600

    update post query hook

--- src/api/blog-api.ts
+++ src/api/blog-api.ts
@@ -8,8 +8,9 @@ type PostsApiResponseProps = {
   headers: AxiosResponseHeaders;
 };
 
-export function useGetPosts(queryParamas) {
+export function useGetPosts(queryParamas, queryConfig: Omit<UseQueryOptions<PostsApiResponseProps, Error>, 'queryKey' | 'queryFn'> = {}) {
   const fetcher = async (queryKey) => {
+
     const response = await api.get(`${queryKey[0]}?_embed`, {
       params: {...queryParamas},
       baseURL: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL + 'wp-json/wp/v2'
@@ -21,5 +22,24 @@ export function useGetPosts(queryParamas) {
   return useQuery<PostsApiResponseProps, Error>({
     queryKey: ['/posts', queryParamas],
     queryFn: ({ queryKey }) => fetcher(queryKey),
+    ...queryConfig
+  });
+}
+
+export function useGetPostsExcludingCurrentPost(id: string, queryConfig: Omit<UseQueryOptions<Post[], Error>, 'queryKey' | 'queryFn'> = {}) {
+  const fetcher = async (queryKey: [string, string]) => {
+    const response = await api.get(`${queryKey[0]}?_embed`, {
+      params: {exclude : [queryKey[1]], per_page: 4},
+      baseURL: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL + 'wp-json/wp/v2'
+    });
+
+    return response.data;
+  };
+
+  return useQuery<Post[], Error>({
+    queryKey: ['/posts', id],
+    queryFn: ({ queryKey }) => fetcher(queryKey as [string, string]),
+    useErrorBoundary: true,
+    ...queryConfig
   });
 }
